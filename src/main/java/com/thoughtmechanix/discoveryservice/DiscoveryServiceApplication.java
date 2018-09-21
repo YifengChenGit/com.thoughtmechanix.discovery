5ef7e92d00683f35d9d4b0db7ea6f1e902b9aed5
Yifeng Chen <ycpostm@gmail.com>
2018-09-20 21:09:26 -0400

    m

--- src/main/java/com/thoughtmechanix/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/com/thoughtmechanix/discoveryservice/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.thoughtmechanix.discovery;
+package com.thoughtmechanix.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryApplication {
+public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryApplication.class, args);
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 }
